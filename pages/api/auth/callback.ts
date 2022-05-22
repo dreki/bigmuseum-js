65528669eaf7cea2e8f407c826cc49689c4e86ab
Sean Gilbertson <sean.gilbertson@gmail.com>
2022-05-22 10:18:42 -0500

    Get access token

--- pages/api/auth/callback.ts
+++ pages/api/auth/callback.ts
@@ -1,37 +1,57 @@
 import { withIronSessionApiRoute } from "iron-session/next";
 import { IRON_SESSION_CONFIG } from "lib/config";
 import { NextApiRequest, NextApiResponse } from "next";
+// Import snoowrap
+import snoowrap, { RedditUser, Subreddit } from 'snoowrap';
 
 export default withIronSessionApiRoute(handler, IRON_SESSION_CONFIG)
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-    // Log request method.
-    console.log(`> method ${req.method}`);
     // Get query parameters.
     const { code, state } = req.query;
     // Log query parameters.
     console.log(`> query ${JSON.stringify(req.query)}`);
-    // Log request
-    // console.log(req);
-    // Log session
-    console.log(req.session);
-    // Log body
-    console.log('> body:');
-    console.log(req.body);
-    // // Generate a state token to prevent request forgery.
-    // // Store it in the session for later validation.
-    // const state = Math.random().toString(36).substring(2);
-    // req.session.state = state;
-    // // Redirect to Reddit auth.
-    // res.writeHead(302, {
-    //     Location: 'https://www.reddit.com/api/v1/authorize?' +
-    //         `client_id=${process.env.REDDIT_CLIENT_ID}` +
-    //         '&response_type=code' +
-    //         `&state=${req.session.state}` +
-    //         '&redirect_uri=http://localhost:3000/api/auth/callback' +
-    //         '&duration=permanent' +
-    //         '&scope=identity'
-    // });
+
+    // POST to Reddit's OAuth2 endpoint to get an access token.
+    const response = await fetch('https://www.reddit.com/api/v1/access_token', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization': `Basic ${Buffer.from(`${process.env.REDDIT_CLIENT_ID}:${process.env.REDDIT_CLIENT_SECRET}`).toString('base64')}`
+        },
+        body: `grant_type=authorization_code&code=${code}&redirect_uri=http://localhost:3000/api/auth/callback`
+    });
+    const { access_token, refresh_token } = await response.json();
+    // Log access token.
+    console.log(`> access_token ${access_token}`);
+    // Log refresh token.
+    console.log(`> refresh_token ${refresh_token}`);
+
+    // Init snoowrap
+    const reddit = new snoowrap({
+        userAgent: 'bigmuseum-app',
+        // accessToken: access_token,
+        clientId: process.env.REDDIT_CLIENT_ID,
+        clientSecret: process.env.REDDIT_CLIENT_SECRET,
+        refreshToken: refresh_token
+    });
+    // Get user info.
+    await reddit.getMe().then(async (user: RedditUser) => {
+        // Log user info.
+        console.log(`> user ${JSON.stringify(user)}`);
+        // Get user's subreddits.
+        // const subreddits: Subreddit[] = await user.getSubscriptions();
+        // Log user's subreddits.
+        // console.log(`> subreddits ${JSON.stringify(subreddits)}`);
+    });
+    // const user: RedditUser = await reddit.getMe();
+    // console.log(user);
+
+    // Get `r/museum` subreddit.
+    // const subreddit: Subreddit = await reddit.getSubreddit('museum');
+    // console.log(subreddit);
+    
+    
     // Return a generic 200
     res.status(200).json({ name: 'John Doe' })
 }
